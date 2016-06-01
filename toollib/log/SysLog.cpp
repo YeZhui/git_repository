74b2372ef1722a13cfb3f1cda7b53d649201b929
xiaoYe <guanyezhui@163.com>
2016-06-01 15:21:53 +0800

    syslog写入暂时完成

--- toollib/log/SysLog.cpp
+++ toollib/log/SysLog.cpp
@@ -1,3 +1,4 @@
+#include <syslog.h>
 #include "SysLog.h"
 
 CSysLog::CSysLog()
@@ -14,11 +15,34 @@ void CSysLog::Init()
 {
 	char szAddress[64] = {0};
 	int  iPort = 0;
-	Connect(szAddress, iPort);
+	if (Connect(szAddress, iPort) != 0)
+	{
+		cout << "openlog failed" << endl;
+		exit(-1);
+	}
 }
 
 int CSysLog::Connect(const char* p_szAddress, int p_iPort)
 {
+	char szProcDir[128] = {0};
+	char szProcName[32] = {0};
+	char *pEndPath = NULL;
+	//得到程序名
+	if (readlink("/proc/self/exe", szProcDir, sizeof(szProcDir)) <= 0 )
+	{
+		return -1;
+	}
+	pEndPath = strrchr( szProcDir, '/');
+	if (pEndPath == NULL)
+	{
+		return -1;
+	}
+
+	++pEndPath;
+
+	strcpy(szProcName, pEndPath);
+
+	openlog(szProcName, LOG_PID, LOG_USER);
 	return 0;
 }
 
@@ -29,11 +53,28 @@ int CSysLog::Reconnect()
 
 int CSysLog::ReadAndPrint()
 {
-	//如果连接断开重连
-	Reconnect();
+	string strLog;
+	while(1)
+	{
+		pthread_mutex_lock(&m_mutex);
+		if (m_queLog.empty())
+		{
+			pthread_cond_wait(&m_cond, &m_mutex);
+		}
+		while(!m_queLog.empty())
+		{
+			strLog.clear();
+			strLog = m_queLog.front();
+			m_queLog.pop();
+			syslog(LOG_INFO, strLog.c_str());	
+		}
+		pthread_mutex_unlock(&m_mutex);
+	}
+	return 0;
 }
 
 void CSysLog::Close()
 {
+	closelog();
 }
 
