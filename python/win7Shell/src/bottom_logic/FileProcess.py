3badf4ada926c7edf9c6a28ce34fc7822988238a
xiaoYe <guanyezhui@163.com>
2017-03-18 17:30:27 +0800

    update fileprocess

--- python/win7Shell/src/bottom_logic/FileProcess.py
+++ python/win7Shell/src/bottom_logic/FileProcess.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import BaseProcess
+import os
 """
 解析各种操作文件的命令
 来一个入参，然后解析后调用对应的函数处理
@@ -29,18 +30,90 @@ def processFileCmd(strFileCmd):
 		processCpCmd(strFileCmd)
 	elif strFileCmd[0:3] == 'mv ':
 		processMvCmd(strFileCmd)
-	else
+	else:
+		BaseProcess.g_ErrorCode = -1
 		BaseProcess.g_ErrorMsg = "The cmmond is not found!"
 		
+def getAbsPath(strCmd):
+	#要考虑字符串长度
+	curPath = os.path.abspath('.')
+	if len(strCmd) == 0:
+		absPath = curPath
+	elif len(strCmd) == 1:
+		if strCmd == '.':
+			absPath = curPath
+		else:
+			BaseProcess.g_ErrorCode = -1
+			BaseProcess.g_ErrorMsg = "This Dir is not exists!"
+			return None
+	elif len(strCmd) >=2 and strCmd[1] != ':':
+		absPath = os.path.join(curPath,strCmd)
+	else:
+		absPath = strCmd
+	return absPath
+		
 def processCdCmd(strFileCmd):
-	pass
+	"""
+	分为当前路径和绝对路径r
+	以字母+冒号开头的都是绝对路径，否则就是相对路径
+	首先识别相对路径，然后转换为绝对路径操作
+	"""
+	strCdCmd = strFileCmd[3:]
+	strCdCmd.lstrip()     #截掉string左边的空格
+	try:
+		absPath = getAbsPath(strCdCmd)
+		if absPath:
+			if os.path.isdir(absPath):
+				os.chdir(absPath)  #切换目录
+				BaseProcess.g_ErrorCode = 0
+			else:
+				BaseProcess.g_ErrorCode = -1
+				BaseProcess.g_ErrorMsg = absPath + ' not a dir'
+	except OSError:
+		BaseProcess.g_ErrorCode = -1
+		BaseProcess.g_ErrorMsg = "OS Error"
 	
 def processMkdirCmd(strFileCmd):
-	pass
-def processTouchCmd(strFileCmd):
-	pass
+	strMkdirCmd = strFileCmd[6:]
+	strMkdirCmd.lstrip()
+	try:
+		absPath = getAbsPath(strCdCmd)
+		if absPath:
+			if os.path.exists(absPath):
+				BaseProcess.g_ErrorCode = -1
+				BaseProcess.g_ErrorMsg = absPath + 'is already exists!'
+			else:
+				os.path.mkdir(absPath)
+				BaseProcess.g_ErrorCode = 0
+	except OSError:
+		BaseProcess.g_ErrorCode = -1
+		BaseProcess.g_ErrorMsg = "OS Error"
+		
 def processRmCmd(strFileCmd):
-	pass
+	strRmCmd = strFileCmd[3:]
+	strRmCmd.lstrip()
+	try:
+		absPath = getAbsPath(strRmCmd)
+		if absPath:
+			if os.path.isdir(absPath):
+				os.rmdir(absPath)
+			elif os.path.exists(absPath):
+				os.remove(absPath)
+			else:
+				BaseProcess.g_ErrorCode = -1
+				BaseProcess.g_ErrorMsg = absPath + 'is not exists!'
+	except OSError:
+		BaseProcess.g_ErrorCode = -1
+		BaseProcess.g_ErrorMsg = "OS Error"
+	BaseProcess.g_ErrorCode = 0
+		
+	
+def processTouchCmd(strFileCmd):
+	strTouchCmd = strFileCmd[6:]
+	strTouchCmd.lstrip()
+	try:
+		absPath = getAbsPath
+
 def processOpenCmd(strFileCmd):
 	pass
 def processCpCmd(strFileCmd):
