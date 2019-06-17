b2af768ddad46138c67850698a5c4b054a409504
YeZhui <guanyezhui@163.com>
2019-06-17 10:52:09 +0800

    购物商城另外创建版本

--- python/shopping_sites/op_xadmin/adminx.py
+++
@@ -1,9 +0,0 @@
-from .models import ops
-import xadmin
-
-# Register your models here.
-
-class opsAdmin(object):
-    list_display = ('opera_name',  'opera_age', )
-    
-xadmin.site.register(ops,  opsAdmin)
--- python/shopping_sites/op_xadmin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpXadminConfig(AppConfig):
-    name = 'op_xadmin'
